e4a7bc95ece2744028b6f8dd800077c7f745e877
Paulo Salvatore <paulo@salvatore.tech>
2019-02-25 13:07:48 -0300

    Aula 1

---
+++ app/src/main/java/br/com/paulosalvatore/pagseguroandroidturmab/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.paulosalvatore.pagseguroandroidturmab;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView tvPrincipal = findViewById(R.id.tvPrincipal);
+        /*final EditText etPrincipal = findViewById(R.id.etPrincipal);
+        final Button btSend = findViewById(R.id.btSend);
+
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = etPrincipal.getText().toString();
+                if (!text.isEmpty()) {
+                    tvPrincipal.setText(text);
+                } else {
+//                    tvPrincipal.setText(R.string.type_some_text);
+                    Toast.makeText(MainActivity.this, R.string.type_some_text, Toast.LENGTH_LONG).show();
+                    etPrincipal.setError(getText(R.string.type_some_text));
+                }
+            }
+        });*/
+    }
+}
