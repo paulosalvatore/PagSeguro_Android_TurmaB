9b0abb8dafb243f00b02c035ab3b951c075e81e4
Paulo Salvatore <paulo@salvatore.tech>
2019-04-03 10:04:35 -0300

    DataBinding inicializado

---
+++ app/src/main/java/br/com/paulosalvatore/pagseguroandroidturmab/Game.kt
@@ -0,0 +1,3 @@
+package br.com.paulosalvatore.pagseguroandroidturmab
+
+data class Game(val name: String)
---
+++ app/src/main/java/br/com/paulosalvatore/pagseguroandroidturmab/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.paulosalvatore.pagseguroandroidturmab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import br.com.paulosalvatore.pagseguroandroidturmab.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+
+        binding.game = Game("Donkey Kong")
+    }
+}
